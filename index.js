b2433c348634b15701fed6aa50992bee510c89cf
tomnyson <tabletkindfire@gmail.com>
2024-03-19 15:23:11 +0700

    source

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-const http = require('http');
-
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('HTTP trigger function processed a request.');
 
-    // Read the name from the query string or the request body.
     const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    if (name) {
-        // Respond with a greeting if a name is provided
-        context.res = {
-            // status defaults to 200
-            body: "Hello, " + name
-        };
-    } else {
-        // Prompt for a name if one isn't provided
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-};
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
